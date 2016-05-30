ac06cc696d1035827f8e0d6943d92429993d5f1c
Maksim Chetverikov <ma.chetverikov@gmail.com>
2016-05-30 20:15:02 +0800

    tests again and small fix

--- test/lib/Watcher.js
+++ test/lib/Watcher.js
@@ -9,8 +9,11 @@ const utils = require('../utils');
 const CollectionsFinder = require('../../lib/finders/CollectionsFinder');
 const CollectionsLoader = require('../../lib/loaders/CollectionsLoader');
 const promisify = require('../../lib/helpers/promises').promisify;
-const cwd = process.cwd();
 const Watcher = require('../../lib/Watcher');
+
+const copy = promisify(fs.copy);
+const remove = promisify(fs.remove);
+const writeFile = promisify(fs.writeFile);
 
 /* eslint prefer-arrow-callback:0 */
 /* eslint max-nested-callbacks:0 */
@@ -31,9 +34,8 @@ describe('lib/Watcher', () => {
 
     it('should recreate collection after change collection.json', () => {
       const caseRoot = 'test/cases/lib/finders/CollectionsFinder/watch';
-      const tmpPath = path.normalize(path.join(caseRoot, '../tmp'));
-      const alreadyPath = path.join(cwd, tmpPath, 'already', 'test-collection.json');
-      const fullTmpPath = path.join(cwd, tmpPath);
+      const tmpPath = getTemporary(caseRoot);
+      const alreadyPath = path.join(tmpPath, 'already', 'test-collection.json');
 
       locator.registerInstance('config', {
         isRelease: true,
@@ -47,7 +49,7 @@ describe('lib/Watcher', () => {
 
       let collections;
 
-      return promisify(fs.copy)(path.join(cwd, caseRoot), fullTmpPath)
+      return copy(caseRoot, tmpPath)
         .then(() => finder.find())
         .then(found => {
           collections = found;
@@ -69,38 +71,36 @@ describe('lib/Watcher', () => {
           });
 
           const collectionJsonContent = JSON.parse(fs.readFileSync(alreadyPath));
-
           collectionJsonContent.timestamp = Date.now();
 
-          fs.writeFileSync(alreadyPath, JSON.stringify(collectionJsonContent));
-
-          return promise;
+          return Promise.all([writeFile(alreadyPath, JSON.stringify(collectionJsonContent)), promise]);
         })
-        .then(() => fs.removeSync(fullTmpPath))
+        .then(() => remove(tmpPath))
         .catch(reason => {
-          fs.removeSync(fullTmpPath);
+          fs.removeSync(tmpPath);
           throw reason;
         });
     });
 
     it('should add new collection', () => {
       const caseRoot = 'test/cases/lib/finders/CollectionsFinder/watch';
+      const tmpPath = getTemporary(caseRoot);
       const anotherCollection = 'test/cases/lib/finders/CollectionsFinder/collections/test1/test2';
-      const fullPathAnother = path.join(process.cwd(), anotherCollection);
-      const fullPathNew = path.join(process.cwd(), caseRoot, 'newCollection');
+      const fullPathNew = path.join(tmpPath, 'newCollection');
 
       locator.registerInstance('config', {
         isRelease: true,
         collectionsGlob: [
-          `${caseRoot}/**/test-collection.json`
+          `${tmpPath}/**/test-collection.json`
         ]
       });
 
       const finder = locator.resolve('collectionsFinder');
       const watcher = locator.resolve('watcher');
-      let collections, watchers;
+      let collections;
 
-      return finder.find()
+      return copy(caseRoot, tmpPath)
+        .then(() => finder.find())
         .then(found => {
           collections = found;
 
@@ -118,26 +118,26 @@ describe('lib/Watcher', () => {
             });
           });
 
-          return Promise.all([promisify(fs.copy)(fullPathAnother, fullPathNew), promise]);
+          return Promise.all([copy(anotherCollection, fullPathNew), promise]);
         })
         .then(data => assert.equal(Object.keys(data[1]).length, 2))
-        .then(() => fs.removeSync(fullPathNew))
+        .then(() => remove(tmpPath))
         .catch(reason => {
-          fs.removeSync(fullPathNew);
+          fs.removeSync(tmpPath);
           throw reason;
         });
     });
 
     it('skip collection with equal name', () => {
       const caseRoot = 'test/cases/lib/finders/CollectionsFinder/watch';
+      const tmpPath = getTemporary(caseRoot);
       const anotherCollection = 'test/cases/lib/finders/CollectionsFinder/collections/test1/test2';
-      const fullPathAnother = path.join(process.cwd(), anotherCollection);
-      const fullPathNew = path.join(process.cwd(), caseRoot, 'newCollection');
-      const fullPathNewDuplicate = path.join(process.cwd(), caseRoot, 'newCollectionDuplicate');
+      const fullPathNew = path.join(tmpPath, 'newCollection');
+      const fullPathNewDuplicate = path.join(tmpPath, 'newCollectionDuplicate');
 
       locator.registerInstance('config', {
         collectionsGlob: [
-          `${caseRoot}/**/test-collection.json`
+          `${tmpPath}/**/test-collection.json`
         ]
       });
 
@@ -146,7 +146,8 @@ describe('lib/Watcher', () => {
 
       let collections;
 
-      return finder.find()
+      return copy(caseRoot, tmpPath)
+        .then(() => finder.find())
         .then(found => {
           collections = found;
 
@@ -170,16 +171,13 @@ describe('lib/Watcher', () => {
             .then(() => finder.find().then(collections => assert.equal(Object.keys(collections).length, 2)));
 
           return Promise.all([
-            promisify(fs.copy)(fullPathAnother, fullPathNew),
-            promisify(fs.copy)(fullPathAnother, fullPathNewDuplicate),
+            copy(anotherCollection, fullPathNew),
+            copy(anotherCollection, fullPathNewDuplicate),
             promise
           ]);
 
         })
-        .then(() => {
-          fs.removeSync(fullPathNew);
-          fs.removeSync(fullPathNewDuplicate);
-        })
+        .then(() => Promise.all([remove(fullPathNew), remove(fullPathNewDuplicate)]))
         .catch(reason => {
           fs.removeSync(fullPathNew);
           fs.removeSync(fullPathNewDuplicate);
@@ -202,9 +200,8 @@ describe('lib/Watcher', () => {
 
     it('should emit "change" collection after change "collection/index.js"', () => {
       const caseRoot = 'test/cases/lib/finders/CollectionsFinder/watch';
-      const tmpPath = path.normalize(path.join(caseRoot, '../tmp'));
-      const alreadyPath = path.join(cwd, tmpPath, 'already', 'index.js');
-      const fullTmpPath = path.join(cwd, tmpPath);
+      const tmpPath = getTemporary(caseRoot);
+      const alreadyPath = path.join(tmpPath, 'already', 'index.js');
 
       locator.registerInstance('config', {
         collectionsGlob: [
@@ -217,7 +214,7 @@ describe('lib/Watcher', () => {
 
       let collections;
 
-      return promisify(fs.copy)(path.join(cwd, caseRoot), fullTmpPath)
+      return copy(caseRoot, tmpPath)
         .then(() => finder.find())
         .then(found => {
           collections = found;
@@ -230,37 +227,33 @@ describe('lib/Watcher', () => {
           const promise = new Promise((fulfill, reject) => {
             logicWatcher.on('error', reject);
             watcher.on('change', data => {
+              logicWatcher.close();
               assert.equal(data.collection.name, collections[Object.keys(collections)[0]].name);
 
-              const Logic = require(alreadyPath);
+              const Logic = require(path.join(process.cwd(), alreadyPath));
               const logic = new Logic();
 
               assert.equal(logic.foo(), 'foo');
-
-              logicWatcher.close();
 
               fulfill();
             });
           });
 
           let collectionJsonContent = fs.readFileSync(alreadyPath).toString();
-
           collectionJsonContent = collectionJsonContent.replace(/blablabla/, 'foo');
 
-          fs.writeFileSync(alreadyPath, collectionJsonContent);
-
-          return promise;
+          return Promise.all([writeFile(alreadyPath, collectionJsonContent), promise]);
         })
-        .then(() => fs.removeSync(fullTmpPath))
+        .then(() => remove(tmpPath))
         .catch(reason => {
-          fs.removeSync(fullTmpPath);
+          fs.removeSync(tmpPath);
           throw reason;
         });
     });
 
     it('should emit "change" collection after add file to collection', () => {
       const caseRoot = 'test/cases/lib/finders/CollectionsFinder/watch';
-      const tmpPath = path.normalize(path.join(caseRoot, '../tmp'));
+      const tmpPath = getTemporary(caseRoot);
       const anotherPath = path.join(tmpPath, 'already', 'AnotherForAlready.js');
       const copyPath = path.join(tmpPath, 'customs', 'AnotherForAlready.js');
       const fullTmpPath = tmpPath;
@@ -276,7 +269,7 @@ describe('lib/Watcher', () => {
 
       let collections;
 
-      return promisify(fs.copy)(path.join(cwd, caseRoot), fullTmpPath)
+      return copy(caseRoot, fullTmpPath)
         .then(() => finder.find())
         .then(found => {
           collections = found;
@@ -289,17 +282,19 @@ describe('lib/Watcher', () => {
           const promise = new Promise((fulfill, reject) => {
             logicWatcher.on('error', reject);
             watcher.on('change', data => {
-              assert.equal(data.filename, anotherPath);
               logicWatcher.close();
-              fulfill();
+              utils.wait(10)
+                .then(() => {
+                  assert.equal(data.filename, anotherPath);
+                  fulfill();
+                })
+                .catch(reject);
             });
           });
 
-          fs.copy(copyPath, anotherPath);
-
-          return promise;
+          return Promise.all([copy(copyPath, anotherPath), promise]);
         })
-        .then(() => fs.removeSync(fullTmpPath))
+        .then(() => remove(fullTmpPath))
         .catch(reason => {
           fs.removeSync(fullTmpPath);
           throw reason;
@@ -308,9 +303,8 @@ describe('lib/Watcher', () => {
 
     it('should emit "unlink" collection after remove logic file', () => {
       const caseRoot = 'test/cases/lib/finders/CollectionsFinder/watch';
-      const tmpPath = path.normalize(path.join(caseRoot, '../tmp'));
-      const alreadyPath = path.join(cwd, tmpPath, 'already', 'index.js');
-      const fullTmpPath = path.join(cwd, tmpPath);
+      const tmpPath = getTemporary(caseRoot);
+      const alreadyPath = path.join(tmpPath, 'already', 'index.js');
 
       locator.registerInstance('config', {
         collectionsGlob: [
@@ -323,7 +317,7 @@ describe('lib/Watcher', () => {
 
       let collections;
 
-      return promisify(fs.copy)(path.join(cwd, caseRoot), fullTmpPath)
+      return copy(caseRoot, tmpPath)
         .then(() => finder.find())
         .then(found => {
           collections = found;
@@ -346,13 +340,11 @@ describe('lib/Watcher', () => {
             .then(() => finder.find())
             .then(found => assert.equal(Object.keys(collections).length, 0));
 
-          fs.removeSync(alreadyPath);
-
-          return promise;
+          return Promise.all([remove(alreadyPath), promise]);
         })
-        .then(() => fs.removeSync(fullTmpPath))
+        .then(() => remove(tmpPath))
         .catch(reason => {
-          fs.removeSync(fullTmpPath);
+          fs.removeSync(tmpPath);
           throw reason;
         });
     });
@@ -395,7 +387,7 @@ describe('lib/Watcher', () => {
 
     it('should add new collection and load', () => {
       const caseRoot = 'test/cases/lib/finders/CollectionsFinder/watch';
-      const tmpPath = path.normalize(path.join(caseRoot, '../tmp'));
+      const tmpPath = getTemporary(caseRoot);
       const anotherCollection = 'test/cases/lib/finders/CollectionsFinder/collections/test1/test2';
       const fullPathNew = path.join(tmpPath, 'newCollection');
 
@@ -404,13 +396,19 @@ describe('lib/Watcher', () => {
       });
 
       const loader = locator.resolve('collectionsLoader');
-      const watcher = locator.resolve('watcher');
+      locator.resolve('watcher');
       const events = locator.resolve('events');
-      let collections;
-      const onCollectionsLoaded = new Promise(fulfill => events.on('allCollectionsLoaded', fulfill));
-      const onReadyWatchers = new Promise(fulfill => events.on('readyWatchers', fulfill));
+      let collections, watchers;
 
-      return promisify(fs.copy)(caseRoot, tmpPath)
+      const onCollectionsLoaded = new Promise(fulfill => events.on('allCollectionsLoaded', fulfill));
+      const onReadyWatchers = new Promise(fulfill => {
+        events.on('readyWatchers', w => {
+          watchers = w;
+          fulfill();
+        });
+      });
+
+      return copy(caseRoot, tmpPath)
         .then(() => Promise.all([loader.load(), onReadyWatchers]))
         .then(() => onCollectionsLoaded)
         .then(loaded => {
@@ -421,7 +419,7 @@ describe('lib/Watcher', () => {
               if (collection.name !== 'cool') {
                 return;
               }
-
+              watchers.forEach(watcher => watcher.close());
               try {
                 assert.equal(Object.keys(loader.getCollectionsByNames()).length, 2);
                 fulfill();
@@ -431,9 +429,9 @@ describe('lib/Watcher', () => {
             });
           });
 
-          return Promise.all([promisify(fs.copy)(anotherCollection, fullPathNew), promise]);
+          return Promise.all([copy(anotherCollection, fullPathNew), promise]);
         })
-        .then(() => fs.removeSync(tmpPath))
+        .then(() => remove(tmpPath))
         .catch(reason => {
           fs.removeSync(tmpPath);
           throw reason;
@@ -442,7 +440,7 @@ describe('lib/Watcher', () => {
 
     it('should reload collection on change', () => {
       const caseRoot = 'test/cases/lib/finders/CollectionsFinder/watch';
-      const tmpPath = path.normalize(path.join(caseRoot, '../tmp'));
+      const tmpPath = getTemporary(caseRoot);
       const alreadyPath = path.join(tmpPath, 'already', 'index.js');
 
       locator.registerInstance('config', {
@@ -455,10 +453,10 @@ describe('lib/Watcher', () => {
 
       const onWatchReady = new Promise(fulfill => events.on('readyWatchers', fulfill));
 
-      return promisify(fs.copy)(caseRoot, tmpPath)
+      return copy(caseRoot, tmpPath)
         .then(() => loader.load())
         .then(() => onWatchReady)
-        .then(() => {
+        .then(watchers => {
           const promise = new Promise((fulfill, reject) => {
             events.on('collectionLoaded', collection => {
               if (collection.name !== 'already') {
@@ -469,6 +467,7 @@ describe('lib/Watcher', () => {
                 const instance = new collection.constructor();
                 assert.equal(instance.foo(), 'foo');
 
+                watchers.forEach(watcher => watcher.close());
                 fulfill();
               } catch (e) {
                 reject(e);
@@ -477,14 +476,11 @@ describe('lib/Watcher', () => {
           });
 
           let collectionJsonContent = fs.readFileSync(alreadyPath).toString();
-
           collectionJsonContent = collectionJsonContent.replace(/blablabla/, 'foo');
 
-          fs.writeFileSync(alreadyPath, collectionJsonContent);
-
-          return promise;
+          return Promise.all([writeFile(alreadyPath, collectionJsonContent), promise]);
         })
-        .then(() => fs.removeSync(tmpPath))
+        .then(() => remove(tmpPath))
         .catch(reason => {
           fs.removeSync(tmpPath);
           throw reason;
@@ -492,3 +488,14 @@ describe('lib/Watcher', () => {
     });
   });
 });
+
+/**
+ * Temporary
+ *
+ * @params {string} root
+ * @returns {string} Name of temporary folder
+ */
+function getTemporary(root) {
+  const rand = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  return path.normalize(path.join(root, `../__tmp__${Date.now()}__${rand}`));
+}
