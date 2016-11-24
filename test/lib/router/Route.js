d2672cabbc0223f98659d739bc87a84aa2a9551a
Maksim Chetverikov <ma.chetverikov@gmail.com>
2016-11-24 12:51:12 +0800

    Different $context when many requests at one time

--- test/lib/router/Route.js
+++ test/lib/router/Route.js
@@ -6,6 +6,7 @@ const ServiceLocator = require('catberry-locator');
 const ContextFactory = require('../../../lib/context/ContextFactory');
 const Route = require('../../../lib/router/Route');
 const CollectionLoader = require('../../mocks/loaders/CollectionsLoader');
+const helpers = require('../../../lib/helpers');
 
 /* eslint prefer-arrow-callback:0 */
 /* eslint max-nested-callbacks:0 */
@@ -34,30 +35,71 @@ describe('lib/router/Route', () => {
     );
   });
 
-  it('#handle', () => {
-    const collections = {
-      Index: {
-        constructor: class Index {
-          foo() {
-            return 'success';
+  describe('#handle', () => {
+    it('Main test', () => {
+      const collections = {
+        Index: {
+          constructor: class Index {
+            foo() {
+              return 'success';
+            }
           }
         }
-      }
-    };
-    const definedRoutes = {Index: '/'};
-    const locator = createLocator(collections, definedRoutes);
-    const handleName = 'foo';
-    const endpoint = 'get /foo/:id/:param';
-    const route = new Route(locator, {name: 'Index', constructor: collections.Index.constructor}, handleName, endpoint);
+      };
+      const definedRoutes = {Index: '/'};
+      const locator = createLocator(collections, definedRoutes);
+      const handleName = 'foo';
+      const endpoint = 'get /foo/:id/:param';
+      const route = new Route(locator, {
+        name: 'Index',
+        constructor: collections.Index.constructor
+      }, handleName, endpoint);
 
-    return locator.resolve('collectionsLoader')
-      .load()
-      .then(() => {
-        return route.handle(request(), null, null);
-      })
-      .then(content => {
-        assert.equal(content, 'success');
-      });
+      return locator.resolve('collectionsLoader')
+        .load()
+        .then(() => {
+          return route.handle(request(), null, null);
+        })
+        .then(content => {
+          assert.equal(content, 'success');
+        });
+    });
+
+    it('should have different $context in collection when the multiple requests at one time', () => {
+      const uuids = [];
+      const collections = {
+        Index: {
+          constructor: class Index {
+            foo() {
+              uuids.push(this.$context.request.uuid);
+            }
+          }
+        }
+      };
+      const definedRoutes = {Index: '/'};
+      const locator = createLocator(collections, definedRoutes);
+      const handleName = 'foo';
+      const endpoint = 'get /foo/:id/:param';
+      const route = new Route(locator, {
+        name: 'Index',
+        constructor: collections.Index.constructor
+      }, handleName, endpoint);
+
+      return locator.resolve('collectionsLoader')
+        .load()
+        .then(() => {
+          return Promise
+            .all([
+              route.handle({request: {uuid: helpers.uuid()}}, null, null),
+              route.handle({request: {uuid: helpers.uuid()}}, null, null),
+              route.handle({request: {uuid: helpers.uuid()}}, null, null)
+            ])
+            .then(() => {
+              assert.equal(uuids.filter((uuid, index, array) => array.indexOf(uuid) === index).length, 3)
+            })
+
+        });
+    });
   });
 });
 
